5215a1413646d56636fd63f616e204c1a217e8c4
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-07-27 20:30:26 -0600

    applying ajax-1st file

--- datasets/trafficIncidents.js
+++ datasets/trafficIncidents.js
@@ -1,7 +1,38 @@
-const data = fetch('https://data.calgary.ca/resource/35ra-9556.json')
+fetch('https://data.calgary.ca/resource/35ra-9556.json')
     .then(response => response.json())
-    .then(data => populateIncidents(data))
+    .then(data => {
+        // Save the data in a variable to use later for filtering
+        const incidentsData = data;
+        // Populate the incidents table with all data initially
+        populateIncidents(incidentsData);
+
+        // Add event listeners to input fields for filtering
+        document.getElementById('search-incident-info').addEventListener('keyup', () => filterIncidents(incidentsData));
+        document.getElementById('search-description').addEventListener('keyup', () => filterIncidents(incidentsData));
+        document.getElementById('search-id').addEventListener('keyup', () => filterIncidents(incidentsData));
+    })
     .catch(error => console.error('Error loading search form:', error));
+
+
+
+function filterIncidents(data) {
+    var searchIncidentInfo = document.getElementById('search-incident-info').value.toLowerCase();
+    var searchDescription = document.getElementById('search-description').value.toLowerCase();
+    var searchId = document.getElementById('search-id').value.toLowerCase();
+
+    var filteredData = data.filter(function (incident) {
+        return (
+            incident.incident_info.toLowerCase().includes(searchIncidentInfo) &&
+            incident.description.toLowerCase().includes(searchDescription) &&
+            incident.id.toLowerCase().includes(searchId)
+        );
+    });
+
+    // Clear the existing table and populate with the filtered data
+    var container = document.getElementById('incident-container');
+    container.innerHTML = '';
+    populateIncidents(filteredData);
+}
 
 
 function createIncidentElement(incident) {
@@ -21,7 +52,7 @@ function createIncidentElement(incident) {
       return row;
 }
 
-// Function to add all incident elements to the container
+
 function populateIncidents(data) {
     var container = document.getElementById('incident-container');
     data.forEach(function (incident) {
